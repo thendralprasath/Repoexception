77bfe2553865a85ba0c3920dfb074c3b804d4416
thendralprasath <thendralprasath@gmail.com>
2021-07-27 22:03:23 +0530

    exception

---
+++ src/Exc.java
@@ -0,0 +1,34 @@
+  import java.io.EOFException;
+              import java.io.IOException;
+public class Exc {
+
+  public static void main(String[] args)
+  {
+      try
+      {
+      System.out.printf("1");
+      int value = 10 / 0;
+       throw new IOException();
+       }
+      catch(EOFException e)
+         {
+    System.out.printf("2");
+      } 
+   catch(ArithmeticException e)
+     {
+    System.out.printf("3");
+       }
+   catch(NullPointerException e)
+    {
+    System.out.printf("4");
+    }
+   catch(IOException e)
+     {
+    System.out.printf("5");
+    }
+   catch(Exception e)
+   {
+    System.out.printf("6");
+      }
+   }
+}
